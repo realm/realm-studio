55afa499f276a15aeccadff07548512887c4bfdc
Kræn Hansen <kraen.hansen@realm.io>
2017-08-18 11:51:34 +0200

    Added source-map support to the main process.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
 import Application from "./main/application";
 
+// Make node understand the source-maps emitted from WebPack.
+if (process.env.NODE_ENV !== "production") {
+  // We must require this the old fasioned way, as this is a dev dependency that might
+  // not be available when the packaged application is shipped, and import statements cannot
+  // be used in a block like this.
+  // tslint:disable-next-line:no-var-requires
+  require("source-map-support").install();
+}
+
 Application.sharedApplication.run();
