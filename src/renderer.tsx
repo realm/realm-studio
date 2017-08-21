75d30204a06c8ee09027a2b736bd4bb215eeb0c6
Kræn Hansen <kraen.hansen@realm.io>
2017-08-21 14:22:36 +0200

    Fixed hot reloading

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,42 +1,33 @@
 import * as electron from "electron";
-import * as querystring from "querystring";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
 
-import { RealmBrowserWindow } from "./windows/RealmBrowserWindow";
+import { CurrentWindow } from "./windows";
 
 import "realm-studio-styles/index.scss";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 // FIXME: see https://github.com/realm/realm-js/issues/818
 const userDataPath = electron.remote.app.getPath("userData");
 process.chdir(userDataPath);
 
 const appElement = document.getElementById("app");
+ReactDOM.render(
+  <AppContainer>
+    <CurrentWindow />
+  </AppContainer>
+, appElement);
 
-function getWindowElement(windowType: WindowType): React.ReactElement<{}> {
-  if (windowType === WindowType.RealmBrowserWindow) {
-    return <RealmBrowserWindow />;
-  } else {
-    throw new Error(`Unexpected window type: ${windowType}`);
-  }
-}
-
-function renderWindowElement(windowType: WindowType) {
-  const windowElement = getWindowElement(windowType);
-  ReactDOM.render(windowElement, appElement);
-}
-
-// Differentiate into some type of window
-const query = querystring.parse(location.search);
-if (typeof(query.windowType) === "string") {
-  const windowType = query.windowType as WindowType;
-  renderWindowElement(windowType);
-  if (module.hot) {
-    module.hot.accept("./windows", () => {
-      console.log("Windows updated!");
-      renderWindowElement(windowType);
-    });
-  }
-} else {
-  throw new Error("Missing a windowType in the query string");
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept("./windows", () => {
+    const NextWindow = require<{CurrentWindow: typeof CurrentWindow}>("./windows").CurrentWindow;
+    ReactDOM.render(
+      <AppContainer>
+        <NextWindow />
+      </AppContainer>
+    , appElement);
+  });
 }
