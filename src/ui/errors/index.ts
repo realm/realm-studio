7eb339174ae2c9ac870c32a9bcd1f2fbb1a35608
Kræn Hansen <kraen.hansen@realm.io>
2017-08-31 16:56:06 +0200

    Allowing callers of show message to override it

--- src/ui/errors/index.ts
+++ src/ui/errors/index.ts
@@ -1,7 +1,10 @@
 import { remote } from "electron";
 
-export const showError = (failedIntent: string, error: any) => {
-  const message = error.title || error.message || "";
+export const showError = (failedIntent: string, error: any, messageOverrides: { [msg: string]: string } = {}) => {
+  let message = error.title || error.message || "";
+  if (message in messageOverrides) {
+    message = messageOverrides[message];
+  }
   // remote.dialog.showErrorBox(failedIntent, message);
   remote.dialog.showMessageBox(remote.getCurrentWindow(), {
     type: "error",
