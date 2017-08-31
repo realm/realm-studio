30a198b9f99085e3eee467d2acf1ced3b596bb7a
Kræn Hansen <kraen.hansen@realm.io>
2017-08-31 16:55:08 +0200

    Making the dev-ros create a random user

--- realm-object-server-dev-server.js
+++ realm-object-server-dev-server.js
@@ -1,8 +1,21 @@
 const RealmObjectServer = require("realm-object-server").Server;
+const Realm = require("realm");
 
 const server = new RealmObjectServer();
 server.start().then(() => {
   console.log(`Realm Object Server started!`);
+
+  const random = Math.floor(Math.random() * 10000);
+  const username = `user-${random}`;
+  const password = "very-secure";
+
+  Realm.Sync.User.register("http://localhost:9080", username, password, (err, user) => {
+    if (err) {
+      console.error(`Couldn't create user ${username}`);
+    } else {
+      console.log(`Created user ${username}:${password}`);
+    }
+  });
 }, (err) => {
   console.error(`Error ${err}`);
 });
