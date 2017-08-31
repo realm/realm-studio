12117501e3a540031344c2b1c9ee8d1e3d934953
Kræn Hansen <kraen.hansen@realm.io>
2017-08-31 16:55:29 +0200

    Making http:// the default schema for connections

--- src/ui/connect-to-server/ConnectToServerContainer.tsx
+++ src/ui/connect-to-server/ConnectToServerContainer.tsx
@@ -32,21 +32,25 @@ export class ConnectToServerContainer extends React.Component<{}, {
 
   public onSubmit = async () => {
     const { url, username, password } = this.state;
+
+    const preparedUrl = this.prepareUrl(url);
+
     this.setState({
       isConnecting: true,
     });
 
-    Realm.Sync.User.login(url, username, password, (err, user) => {
+    Realm.Sync.User.login(preparedUrl, username, password, (err, user) => {
       this.setState({
         isConnecting: false,
       });
 
       if (err) {
-        // TODO: Find a good way to display this error to the user
-        showError(`Couldn't connect to Realm Object Server`, err);
+        showError(`Couldn't connect to Realm Object Server`, err, {
+          "Failed to fetch": "Could not reach the server",
+        });
       } else {
         // TODO: Tell the main process that we can connect
-        console.log(`Logged in as ${user}`);
+        console.log(`Logged in as ${user}`, user);
       }
     });
   }
@@ -67,5 +71,13 @@ export class ConnectToServerContainer extends React.Component<{}, {
     this.setState({
       password: e.target.value,
     });
+  }
+
+  private prepareUrl(url: string) {
+    if (url.indexOf("http") !== 0) {
+      return `http://${url}`;
+    } else {
+      return url;
+    }
   }
 }
