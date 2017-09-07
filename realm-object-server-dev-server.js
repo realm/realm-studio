26826655c7642ca78a40db8b54b229cfe7324609
Kræn Hansen <kraen.hansen@realm.io>
2017-09-07 14:13:45 +0200

    Creating an admin user instead of a random username

--- realm-object-server-dev-server.js
+++ realm-object-server-dev-server.js
@@ -1,17 +1,20 @@
-const RealmObjectServer = require("realm-object-server").Server;
+const path = require("path");
+const ROS = require("realm-object-server");
 const Realm = require("realm");
 
-const server = new RealmObjectServer();
+const server = new ROS.Server({
+  dataPath: path.resolve("data"),
+});
+server.addService(new ROS.UsernamePasswordAuthService());
 server.start().then(() => {
   console.log(`Realm Object Server started!`);
 
-  const random = Math.floor(Math.random() * 10000);
-  const username = `user-${random}`;
-  const password = "very-secure";
+  const username = "admin";
+  const password = "admin";
 
   Realm.Sync.User.register("http://localhost:9080", username, password, (err, user) => {
     if (err) {
-      console.error(`Couldn't create user ${username}`);
+      console.error(`Couldn't create user ${username}`, err);
     } else {
       console.log(`Created user ${username}:${password}`);
     }
