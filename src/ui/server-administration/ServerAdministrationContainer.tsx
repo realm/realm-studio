bd416fa78deaf0356cd612572c347763d30c01e7
Kræn Hansen <kraen.hansen@realm.io>
2017-09-09 01:43:57 +0200

    Creating a user from either a token or a username+password

--- src/ui/server-administration/ServerAdministrationContainer.tsx
+++ src/ui/server-administration/ServerAdministrationContainer.tsx
@@ -2,16 +2,16 @@ import * as electron from "electron";
 import * as React from "react";
 import * as Realm from "realm";
 
-import { showServerAdministration } from "../../actions";
+import {
+  IAdminTokenCredentials,
+  IServerAdministrationOptions,
+  IUsernamePasswordCredentials,
+} from "../../windows/WindowType";
 import { showError } from "../reusable/errors";
 
 import { ServerAdministration, Tab } from "./ServerAdministration";
 
-export class ServerAdministrationContainer extends React.Component<{
-  url: string,
-  username: string,
-  password: string,
-}, {
+export class ServerAdministrationContainer extends React.Component<IServerAdministrationOptions, {
   activeTab: Tab,
   user: Realm.Sync.User | null,
 }> {
@@ -25,15 +25,22 @@ export class ServerAdministrationContainer extends React.Component<{
   }
 
   public componentDidMount() {
-    Realm.Sync.User.login(this.props.url, this.props.username, this.props.password, (err, user) => {
-      if (err) {
-        showError(`Couldn't connect to Realm Object Server`, err, {
-          "Failed to fetch": "Could not reach the server",
-        });
-      } else {
-        this.setState({ user });
-      }
-    });
+    if ("token" in this.props.credentials) {
+      const credentials = this.props.credentials as IAdminTokenCredentials;
+      const user = Realm.Sync.User.adminUser(credentials.token, this.props.url);
+      this.setState({ user });
+    } else if ("username" in this.props.credentials && "password" in this.props.credentials) {
+      const credentials = this.props.credentials as IUsernamePasswordCredentials;
+      Realm.Sync.User.login(this.props.url, credentials.username, credentials.password, (err, user) => {
+        if (err) {
+          showError(`Couldn't connect to Realm Object Server`, err, {
+            "Failed to fetch": "Could not reach the server",
+          });
+        } else {
+          this.setState({ user });
+        }
+      });
+    }
   }
 
   public render() {
