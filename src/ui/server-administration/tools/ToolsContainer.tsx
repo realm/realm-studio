82883296f2e3d3b67d94d94f7972484e50ff69cd
Kræn Hansen <kraen.hansen@realm.io>
2017-09-26 16:22:44 +0200

    Removed the implementation of generating mocked users and realms

--- src/ui/server-administration/tools/ToolsContainer.tsx
+++ src/ui/server-administration/tools/ToolsContainer.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as Realm from "realm";
 
-import { createFakeUsersAndRealms } from "../../../services/mocked-ros";
 import { getAdminRealm } from "../../../services/ros";
 
 import { Tools } from "./Tools";
@@ -15,17 +14,7 @@ export class ToolsContainer extends React.Component<{
   }
 
   public onGenerateMockUserAndRealms = async () => {
-    const userCount = 1000;
-    const realmCount = 1000;
-
-    const adminRealm = await getAdminRealm(this.props.user);
-    const { createdUsers, createdRealms } = createFakeUsersAndRealms({
-      adminRealm,
-      userCount,
-      realmCount,
-    });
-
-    alert(`Created ${createdUsers} users and ${createdRealms}`);
+    throw new Error("No longer implemented ...");
   }
 
 }
