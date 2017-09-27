65d1407794b199ff553d7b751349c6f52c40d02a
Kræn Hansen <kraen.hansen@realm.io>
2017-09-27 20:01:06 +0200

    Added comment, a README and npm rebuild

--- releasing/linux/build-inside.sh
+++ releasing/linux/build-inside.sh
@@ -5,4 +5,8 @@
 # Make npm rebuild not download realm binaries
 export NPM_CONFIG_realm_download_binaries=0
 
+# Installing the package will rebuild dependent binaries for Electron on Linux
 npm install
+
+# Rebuild the dependencies for node on Linux - needed if run from a machine that already installed the package.
+npm rebuild
