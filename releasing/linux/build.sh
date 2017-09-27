401da0f2888cf6fccfb4bfac23fb673fa6ba7c31
Kræn Hansen <kraen.hansen@realm.io>
2017-09-27 17:17:25 +0200

    Added scripts to install the app and it's dependencies rebuild in a docker image

---
+++ releasing/linux/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Let's figure out where this script is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check that the packagecloud token is sat, or exit
+if [ -z "$PACKAGECLOUD_TOKEN" ]; then
+    echo "Need to set PACKAGECLOUD_TOKEN"
+    exit 1
+fi
+
+# Use the same dependencies as the current version of realm
+REALM_PATH=$(dirname $(node -p "require.resolve('realm')"))/..
+source $REALM_PATH/dependencies.list
+
+# Build the docker image
+docker build -t realm-studio \
+  --build-arg PACKAGECLOUD_URL=https://${PACKAGECLOUD_TOKEN}:@packagecloud.io/install/repositories/realm/sync-devel \
+  --build-arg REALM_SYNC_VERSION=${REALM_SYNC_VERSION} \
+  $DIR
+
+# Build the dependencies for electron
+docker run -v $DIR/../..:/src -t realm-studio
