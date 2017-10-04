36579c3718e64ed3272ec90404855071f8b63d1c
Kræn Hansen <mail@kraenhansen.dk>
2017-10-04 18:19:24 +0200

    Faster CI (#209)
    * WIP
    
    * Installing packages when building the image
    
    * Adding in the package-lock and not installing when running now
    
    * package(-lock).json is in the parent dir
    
    * Moved the Dockerfile one level up
    
    * WIP
    
    * Linking in the node_modules
    
    * Pushing the image to the registry
    
    * Missing def
    
    * Double quoting
    
    * Forgot a named param
    
    * Empty commit
    
    * Split up the build and test stages
    
    * Adding an error message when the package-lock changed
    
    * Committing an updated package-lock
    
    * Better error messaging
    
    * Adding a check script to test if package was updated without updating the lock
    
    * Removed a testing script that was not longer needed
    
    * Fixing the lock
    
    * Test

---
+++ checking/package-lock.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const semver = require('semver');
+
+const package = require('../package.json');
+const packageLock = require('../package-lock.json');
+
+function checkVersion(name, version, lockPackage) {
+  assert(
+    semver.satisfies(lockPackage.version, version),
+    `Locks version (${lockPackage.version}) of ${name} didn't satisfy the package ${version}`,
+  );
+}
+
+try {
+  assert.equal(package.name, packageLock.name, "Name changed");
+  assert.equal(package.version, packageLock.version, "Version changed");
+  // Check that all package.json dependencies are semantically compatible with the lock
+  Object.keys(package.dependencies).forEach(name => {
+    const version = package.dependencies[name];
+    // Check if the package exists in the lock
+    const lockPackage = packageLock.dependencies[name];
+    assert(lockPackage, `"${name}" is missing from the lock`);
+    assert(!lockPackage.dev, `"${name}" is now a production dependency`);
+    checkVersion(name, version, lockPackage);
+  });
+  // And the same for the devDependencies
+  Object.keys(package.devDependencies).forEach(name => {
+    const version = package.devDependencies[name];
+    // Check if the package exists in the lock
+    const lockPackage = packageLock.dependencies[name];
+    assert(lockPackage, `"${name}" is missing from the lock`);
+    // We shouldn't check that lockPackage.dev is true - because another production
+    // dependency might depend on this package.
+    checkVersion(name, version, lockPackage);
+  });
+} catch(err) {
+  if (err instanceof assert.AssertionError) {
+    console.error("Package changed, but lock wasn't updated:");
+    console.error(err.message);
+  } else {
+    console.error(err);
+  }
+  process.exit(-1);
+}
+
+console.log("Looking good!");
