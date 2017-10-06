28a0dd99110efe5a634c9098abb69e2577a245ba
Kræn Hansen <kraen.hansen@realm.io>
2017-10-06 11:55:56 +0200

    Build on windows

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -4,56 +4,56 @@ const webpack = require('webpack');
 const { CheckerPlugin } = require("awesome-typescript-loader");
 const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
 
-const isProduction = process.env.NODE_ENV === "production";
-// console.log("Running in", isProduction ? "production" : "development", "mode");
+module.exports = (env) => {
+  const isProduction = env && env.NODE_ENV === "production";
+  // console.log("Running in", isProduction ? "production" : "development", "mode");
 
-const externals = [
-  nodeExternals({
-    // Anyting related to webpack, we want to keep in the bundle
-    whitelist: [
-      /webpack(\/.*)?/,
-    ]
-  }),
-];
-
-const node = {
-  // This will make __dirname equal the bundles path
-  __dirname: false
-};
-
-const resolve = {
-  alias: {
-    "realm-studio-styles": path.resolve(__dirname, "styles"),
-    "realm-studio-svgs": path.resolve(__dirname, "static/svgs")
-  },
-  extensions: [".ts", ".tsx", ".js", ".jsx", ".html", ".scss", ".svg"],
-};
-
-const baseConfig = {
-  devtool: !isProduction ? "inline-source-map" : false,
-  externals,
-  module: {
-    rules: []
-  },
-  node,
-  output: {
-    path: path.resolve(__dirname, "build"),
-  },
-  plugins: [
-    // @see https://github.com/s-panferov/awesome-typescript-loader#configuration on why CheckerPlugin is needed
-    new CheckerPlugin(),
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+  const externals = [
+    nodeExternals({
+      // Anyting related to webpack, we want to keep in the bundle
+      whitelist: [
+        /webpack(\/.*)?/,
+      ]
     }),
-    new SpriteLoaderPlugin(),
-  ].concat(isProduction ? [
-    // Plugins for production
-  ] : [
-    // Plugins for development
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ]),
-  resolve
-};
+  ];
 
-module.exports = baseConfig;
+  const node = {
+    // This will make __dirname equal the bundles path
+    __dirname: false
+  };
+
+  const resolve = {
+    alias: {
+      "realm-studio-styles": path.resolve(__dirname, "styles"),
+      "realm-studio-svgs": path.resolve(__dirname, "static/svgs")
+    },
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".html", ".scss", ".svg"],
+  };
+
+  return {
+    devtool: !isProduction ? "inline-source-map" : false,
+    externals,
+    module: {
+      rules: []
+    },
+    node,
+    output: {
+      path: path.resolve(__dirname, "build"),
+    },
+    plugins: [
+      // @see https://github.com/s-panferov/awesome-typescript-loader#configuration on why CheckerPlugin is needed
+      new CheckerPlugin(),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      }),
+      new SpriteLoaderPlugin(),
+    ].concat(isProduction ? [
+      // Plugins for production
+    ] : [
+      // Plugins for development
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+    ]),
+    resolve
+  };
+};
