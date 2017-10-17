624a87637ccca8c835293188e2c83b2d685a8295
Kræn Hansen <mail@kraenhansen.dk>
2017-10-17 16:46:34 +0200

    Preparing releases continued (#278)
    * Adding newlines to the package and -lock
    
    * No need to check if the branch exists - it will fail if it does

--- scripts/prepare-next-release.js
+++ scripts/prepare-next-release.js
@@ -88,29 +88,15 @@ async function writeVersion(nextVersion) {
     version: nextVersion,
   };
   // Write back the two files
-  fs.writeFileSync(packageJsonPath, JSON.stringify(nextPackageJson, undefined, 2));
-  fs.writeFileSync(packageLockJsonPath, JSON.stringify(nextPackageLockJson, undefined, 2));
+  fs.writeFileSync(packageJsonPath, JSON.stringify(nextPackageJson, undefined, 2) + "\n");
+  fs.writeFileSync(packageLockJsonPath, JSON.stringify(nextPackageLockJson, undefined, 2) + "\n");
   // And the package lock json
   console.log(`Bumped version to ${nextVersion} - saved package.json and package-lock.json`);
 }
 
-async function doesBranchExist(name) {
-  try {
-    await promised(callback => git.show(['--quiet', `refs/heads/${name}`], callback));
-    // If it succeeds - the branch did exist
-    return true;
-  } catch (err) {
-    // If it fails - the branch did not exist
-    return false;
-  }
-}
-
 async function createBranch(nextVersion) {
   const branchName = `prepare-${nextVersion}`;
-  // Check if the branch already exists
-  const branchExists = await doesBranchExist(branchName);
-  assert(!branchExists, `The release branch ${branchName} already exists!`);
-  // Create the branch
+  // Create the branch - it will fail if it already exists
   await promised(callback => git.checkoutBranch(branchName, 'master', callback));
 }
 
