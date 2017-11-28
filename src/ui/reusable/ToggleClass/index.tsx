58a8ce10534f6b9ba8700a7600cdf758689c7b79
Ivan Coronado Moreno <ivacormor@gmail.com>
2017-11-28 11:12:46 +0100

    feat (ToggleClass): Add new component

---
+++ src/ui/reusable/ToggleClass/index.tsx
@@ -0,0 +1,22 @@
+import * as classnames from 'classnames';
+import * as React from 'react';
+
+export interface IProps {
+  isOpen: boolean;
+  classUsedWhenOpened: string;
+  classUsedWhenClosed: string;
+  children: React.ReactNode;
+}
+
+export const ToggleClass = ({
+  isOpen,
+  children,
+  classUsedWhenOpened,
+  classUsedWhenClosed,
+}: IProps) => (
+  <div
+    className={classnames(isOpen && classUsedWhenOpened, classUsedWhenClosed)}
+  >
+    {children}
+  </div>
+);
--- src/ui/server-administration/sidebar/Sidebar.tsx
+++ src/ui/server-administration/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
-import * as classnames from 'classnames';
 import * as React from 'react';
+import { ToggleClass } from '../../reusable/ToggleClass';
 import './Sidebar.scss';
 
 export interface IProps {
@@ -7,10 +7,12 @@ export interface IProps {
   children: React.ReactNode;
 }
 
-const Sidebar = ({ isOpen, children }: IProps) => (
-  <div className={classnames(isOpen && 'Sidebar--active', 'Sidebar')}>
+export const Sidebar = ({ isOpen, children }: IProps) => (
+  <ToggleClass
+    isOpen={isOpen}
+    classUsedWhenOpened="Sidebar--active"
+    classUsedWhenClosed="Sidebar"
+  >
     {children}
-  </div>
+  </ToggleClass>
 );
-
-export default Sidebar;
