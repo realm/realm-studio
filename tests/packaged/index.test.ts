fe2023768966003fd287dba9b188b3405081d27f
Kræn Hansen <kraen.hansen@realm.io>
2017-12-13 17:00:36 +0100

    Added the an initial signup form test

--- tests/packaged/index.test.ts
+++ tests/packaged/index.test.ts
@@ -12,19 +12,27 @@ describe('The packaged application', function() {
 
   before(() => {
     app = getApplication();
+    // TODO: Reset the localStorage
   });
 
   beforeEach(async () => {
     await app.start();
+    // Open the dev tools
+    app.webContents.openDevTools();
   });
 
   afterEach(() => {
     return app.stop();
   });
 
-  it('starts', done => {
+  it('starts', () => {
     assert(app.isRunning);
-    app.webContents.openDevTools();
-    setTimeout(done, 5000);
+  });
+
+  it('shows the signup form', () => {
+    const FORM_SELECTOR = '.Greeting__SignupOverlay form';
+    app.client.waitForVisible(FORM_SELECTOR);
+    app.client.element(FORM_SELECTOR).setValue('#email', 'tester@realm.io');
+    app.client.submitForm(FORM_SELECTOR);
   });
 });
