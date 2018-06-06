c1fb73921f96c1c7e484b1a80cb0f0f84f4a7324
Kræn Hansen <kraen.hansen@realm.io>
2018-06-06 12:03:28 +0200

    Changed a .ts file

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,4 @@
-// TODO: Replace this with a Realm as soon as we don't need to CWD in the renderer.
+// TODO: Replace this with a Realm as soon as we don't need to CWD in the renderer
 // @see https://github.com/realm/realm-studio/blob/master/src/renderer.tsx#L15-L30
 
 import ElectronStore = require('electron-store');
