fafa1cf46d3d4a2965d7093a6eb1de8c1193c9e2
Kræn Hansen <kraen.hansen@realm.io>
2018-06-06 11:46:01 +0200

    Adding a script that will fix the headers of files

---
+++ scripts/fix-headers.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const { resolve, extname } = require('path');
+
+const EXTENSIONS_WITH_HEADERS = ['.ts', '.tsx'];
+
+function readHeader() {
+  const headerPath = resolve(__dirname, 'header.txt');
+  const header = fs.readFileSync(headerPath, 'utf8');
+  // Split up the header to identify the first and last lines
+  const lines = header.trim().split('\n');
+  const firstLine = lines[0];
+  const lastLine = lines[lines.length - 1];
+  return {
+    lines,
+    firstLine,
+    lastLine,
+  };
+}
+
+const header = readHeader();
+
+function processFolder(path) {
+  const subPaths = fs.readdirSync(path);
+  for (const subPath of subPaths) {
+    const absoluteSubPath = resolve(path, subPath);
+    const stat = fs.statSync(absoluteSubPath);
+    if (stat.isDirectory()) {
+      processFolder(absoluteSubPath);
+    } else if (stat.isFile()) {
+      processFile(absoluteSubPath);
+    }
+  }
+}
+
+function processFile(path) {
+  const extension = extname(path);
+  if (EXTENSIONS_WITH_HEADERS.indexOf(extension) > -1) {
+    const contents = fs.readFileSync(path, 'utf8');
+    const newContents = processFileContents(contents);
+    fs.writeFileSync(path, newContents);
+  }
+}
+
+function processFileContents(contents) {
+  const lines = contents.split('\n');
+  const headerLines = locateHeader(lines);
+  if (headerLines.start === -1 || headerLines.end === -1) {
+    // Prepend the header to the file contents
+    return [...header.lines, '', ...lines].join('\n');
+  } else {
+    // Modify the lines to add the latest header
+    lines.splice(
+      headerLines.start,
+      headerLines.end + 1,
+      ...header.lines
+    ).join('\n');
+    // Return the new lines
+    return lines.join('\n');
+  }
+}
+
+function locateHeader(lines) {
+  return {
+    start: lines.indexOf(header.firstLine),
+    end: lines.lastIndexOf(header.lastLine),
+  };
+}
+
+const srcPath = resolve(__dirname, '../src');
+processFolder(srcPath);
