9ae9d34434d76a0e5acb1f7fae61027be91a533e
Kræn Hansen <mail@kraenhansen.dk>
2018-06-06 13:12:22 +0200

    With headers (#813)
    * Adding a script that will fix the headers of files
    
    * Fixing the headers when staging a .ts file
    
    * Adding a comment to a .ts file
    
    * Changed a .ts file
    
    * Changed two ts files
    
    * Changed two files at the same time
    
    * Running on multiple specific files
    
    * Modified two files
    
    * Adding the header to all *.ts and *.tsx files
    
    * Adding headers to .scss files too

---
+++ scripts/fix-headers.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const { resolve, extname } = require('path');
+
+const EXTENSIONS_WITH_HEADERS = ['.ts', '.tsx', '.scss'];
+
+function readHeader() {
+  const headerPath = resolve(__dirname, 'header.txt');
+  const header = fs.readFileSync(headerPath, 'utf8');
+  // Split up the header to identify the first and last lines
+  const lines = header.trim().split('\n');
+  const firstLine = lines[0];
+  const lastLine = lines[lines.length - 1];
+  return {
+    lines,
+    firstLine,
+    lastLine,
+  };
+}
+
+const header = readHeader();
+
+function processDirectory(path) {
+  const subPaths = fs.readdirSync(path);
+  for (const subPath of subPaths) {
+    const absoluteSubPath = resolve(path, subPath);
+    const stat = fs.statSync(absoluteSubPath);
+    if (stat.isDirectory()) {
+      processDirectory(absoluteSubPath);
+    } else if (stat.isFile()) {
+      processFile(absoluteSubPath);
+    }
+  }
+}
+
+function processFile(path) {
+  const extension = extname(path);
+  if (EXTENSIONS_WITH_HEADERS.indexOf(extension) > -1) {
+    const contents = fs.readFileSync(path, 'utf8');
+    const newContents = processFileContents(contents);
+    fs.writeFileSync(path, newContents);
+  }
+}
+
+function processFileContents(contents) {
+  const lines = contents.split('\n');
+  const headerLines = locateHeader(lines);
+  if (headerLines.start === -1 || headerLines.end === -1) {
+    // Prepend the header to the file contents
+    return [...header.lines, '', ...lines].join('\n');
+  } else {
+    // Modify the lines to add the latest header
+    lines.splice(
+      headerLines.start,
+      headerLines.end + 1,
+      ...header.lines
+    ).join('\n');
+    // Return the new lines
+    return lines.join('\n');
+  }
+}
+
+function locateHeader(lines) {
+  return {
+    start: lines.indexOf(header.firstLine),
+    end: lines.lastIndexOf(header.lastLine),
+  };
+}
+
+if(process.argv.length === 2) {
+  const srcPath = resolve(__dirname, '../src');
+  processDirectory(srcPath);
+} else {
+  // The script was asked to run on specific file(s)
+  const [_node, _script, ...paths] = process.argv;
+  for (const path of paths) {
+    processFile(path);
+  }
+}
