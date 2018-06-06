71db9ec83f0595311fb0c252bbf91ea242462727
Kræn Hansen <kraen.hansen@realm.io>
2018-06-06 12:04:39 +0200

    Changed two ts files

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-// Constants that can be used from either the main or render processes
+// Constants that can be used from either the main or render processes.
 export const CLOUD_PROTOCOL = 'x-realm-cloud';
 export const STUDIO_PROTOCOL = 'x-realm-studio';
