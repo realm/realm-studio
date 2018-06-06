16609ca95ccd9a85fe2275f9aa60cebeb76cd0b6
Kræn Hansen <kraen.hansen@realm.io>
2018-06-06 12:09:16 +0200

    Adding the header to all *.ts and *.tsx files

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2018 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
 // TODO: Replace this with a Realm as soon as we don't need to CWD in the renderer.
 // @see https://github.com/realm/realm-studio/blob/master/src/renderer.tsx#L15-L30
 
