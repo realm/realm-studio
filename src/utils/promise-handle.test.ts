d48981966b5511a8dc6617d9050bddecad096874
Kræn Hansen <kraen.hansen@realm.io>
2018-06-08 15:44:42 +0200

    Creating a promise-handle utility

---
+++ src/utils/promise-handle.test.ts
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2018 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+import * as assert from 'assert';
+
+import { createPromiseHandle } from './promise-handle';
+
+describe('promise handle', () => {
+  it('can create a handle', () => {
+    const handle = createPromiseHandle();
+    assert.equal(typeof handle.resolve, 'function');
+    assert.equal(typeof handle.reject, 'function');
+    assert.equal(typeof handle.promise, 'object');
+    assert(handle.promise instanceof Promise);
+  });
+
+  it('can resolve the promise (calling cleanup)', done => {
+    let cleanedUp = false;
+    const handle = createPromiseHandle(() => {
+      cleanedUp = true;
+    });
+    handle.promise.then(result => {
+      assert.equal(result, 'w00t');
+      assert.equal(cleanedUp, true);
+      done();
+    });
+    process.nextTick(() => {
+      handle.resolve('w00t');
+    });
+  });
+
+  it('can reject the promise (calling cleanup)', done => {
+    let cleanedUp = false;
+    const handle = createPromiseHandle(() => {
+      cleanedUp = true;
+    });
+    handle.promise.catch(result => {
+      assert.equal(result, 'w00t');
+      assert.equal(cleanedUp, true);
+      done();
+    });
+    process.nextTick(() => {
+      handle.reject('w00t');
+    });
+  });
+});
---
+++ src/utils/promise-handle.ts
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2018 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+// This utility can be used to create a promise and externalize the resolve and reject methods to the caller
+
+export interface IPromiseHandle<T> {
+  promise: Promise<T>;
+  resolve: (result: T) => void;
+  reject: (reason: any) => void;
+}
+
+export const createPromiseHandle = <T extends any>(
+  cleanupCallback?: () => void,
+) => {
+  const handle: Partial<IPromiseHandle<T>> = {};
+  handle.promise = new Promise<T>((resolve, reject) => {
+    handle.resolve = resolve;
+    handle.reject = reject;
+  }).then(
+    (value: T) => {
+      if (cleanupCallback) {
+        cleanupCallback();
+      }
+      return value;
+    },
+    (reason: any) => {
+      if (cleanupCallback) {
+        cleanupCallback();
+      }
+      throw reason;
+    },
+  );
+  return handle as IPromiseHandle<T>;
+};
