f7c09180c07c6109e1a0247e0a5fc4763ad3b333
Kræn Hansen <kraen.hansen@realm.io>
2018-06-15 17:40:10 +0200

    Assuming production if not development

--- src/sentry.js
+++ src/sentry.js
@@ -1,7 +1,7 @@
 // Initialize the Sentry for early error catching
 // This is external to the bundle and will be copied by WebPack to catch any errors that may happen early
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV !== 'development';
 
 const Sentry = require('@sentry/electron');
 Sentry.init({
