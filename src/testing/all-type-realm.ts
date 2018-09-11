fc50c0f251035ece454a1db7957865ecd9b26bc6
Kræn Hansen <kraen.hansen@realm.io>
2018-09-11 09:15:33 +0200

    Testing a browser opening a Realm with all types

---
+++ src/testing/all-type-realm.ts
@@ -0,0 +1,107 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2018 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+import { resolve } from 'path';
+import * as Realm from 'realm';
+import * as tmp from 'tmp';
+
+import { TYPES as primitiveTypes } from '../ui/RealmBrowser/primitives';
+
+const CustomClass: Realm.ObjectSchema = {
+  name: 'CustomClass',
+  primaryKey: 'id',
+  properties: {
+    id: 'string',
+    number: 'int',
+  },
+};
+
+const optionalVariations = [{ optional: true }, { optional: false }];
+
+function capitalize(s: string | undefined) {
+  if (s && s.length >= 1) {
+    return s.charAt(0).toUpperCase() + s.substr(1);
+  } else {
+    return s;
+  }
+}
+
+function generatePropertyName(property: Realm.ObjectSchemaProperty) {
+  let result = property.optional ? 'optional' : 'required';
+  if (property.type === 'list') {
+    result += `ListOf${capitalize(property.objectType)}`;
+  } else {
+    result += capitalize(property.type);
+  }
+  return result;
+}
+
+function generateSchema() {
+  const schema: Realm.ObjectSchema[] = [];
+  const types = [...primitiveTypes, 'CustomClass'];
+  // Generate permutations of type and optional variations
+  for (const type of types) {
+    const properties: { [name: string]: Realm.ObjectSchemaProperty } = {};
+    // Elements or list of elements
+    const typeVariations = [{ type }, { type: 'list', objectType: type }];
+    for (const typeVariation of typeVariations) {
+      for (const optionalVariation of optionalVariations) {
+        const property = { ...typeVariation, ...optionalVariation };
+        // Skip the optional list of CustomClass
+        if (
+          property.type === 'list' &&
+          property.objectType === 'CustomClass' &&
+          property.optional
+        ) {
+          continue;
+        }
+        const name = generatePropertyName(property);
+        properties[name] = property;
+      }
+    }
+    // Push a class into the schema with the type related properties
+    schema.push({ name: capitalize(type) + 'Properties', properties });
+  }
+  // Add the CustomClass at the end
+  schema.push(CustomClass);
+  // Return the list of object schemas
+  return schema;
+}
+
+export interface ITestRealm extends Realm {
+  closeAndDelete: () => void;
+}
+
+export const create = () => {
+  const schema = generateSchema();
+  const tempDirectory = tmp.dirSync();
+  const path = resolve(tempDirectory.name, 'all-types.realm');
+  const realm = new Realm({ path, schema }) as ITestRealm;
+  // Implement a method to close and delete the Realm
+  realm.closeAndDelete = () => {
+    realm.close();
+    // Delete the Realm file
+    Realm.deleteFile({
+      path: realm.path,
+    });
+    // Delete the temporary directory
+    tempDirectory.removeCallback();
+  };
+  // Return the Realm
+  return realm;
+};
---
+++ src/ui/RealmBrowser/index.test.tsx
@@ -0,0 +1,161 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2018 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+import * as assert from 'assert';
+import * as Electron from 'electron';
+import { resolve } from 'path';
+import { Application } from 'spectron';
+import * as fakeDialog from 'spectron-fake-dialog';
+
+import {
+  create as createAllTypeRealm,
+  ITestRealm,
+} from '../../testing/all-type-realm';
+
+// When electron is required from Node.js, it returns a string with the path of the electron executable
+const electronPath: string = Electron as any;
+const appPath = resolve(__dirname, '../../..');
+
+// We need to use a non-arrow functions to adjust the suite timeout
+// tslint:disable-next-line:only-arrow-functions
+describe('RealmBrowser', function() {
+  this.timeout(10000);
+
+  let app: Application;
+  let realm: ITestRealm;
+
+  before(async () => {
+    realm = createAllTypeRealm();
+    app = new Application({
+      path: electronPath,
+      args: [
+        // The app directory path
+        appPath,
+        // The path of the Realm to open
+        realm.path,
+      ],
+    });
+    // Apply the modifications that will allow us to mock dialogs
+    fakeDialog.apply(app);
+    // Start the app
+    await app.start();
+  });
+
+  after(async () => {
+    if (app && app.isRunning()) {
+      await app.stop();
+    }
+    if (realm) {
+      // Close the Realm file
+      realm.closeAndDelete();
+    }
+  });
+
+  describe('opening a Realm file', () => {
+    before(async () => {
+      // Mock the open dialog to return the Realm path
+      fakeDialog.mock([
+        {
+          method: 'showOpenDialog',
+          value: [realm.path],
+        },
+      ]);
+      // Await the Greeting window
+      assert.equal(await app.client.getWindowCount(), 1);
+      assert.equal(await app.client.getTitle(), 'Realm Studio');
+      // Click on the button to open a Realm file
+      await app.client.click('button=Open Realm file');
+      await app.client.windowByIndex(1);
+    });
+
+    it('opens the Realm Browser', async () => {
+      // Wait for the browser window to open and change focus to that
+      assert.equal(await app.client.getWindowCount(), 2);
+      await app.client.waitForExist('span=Classes');
+    });
+
+    const classNames = [
+      'BoolProperties',
+      'CustomClassProperties',
+      'DataProperties',
+      'DateProperties',
+      'DoubleProperties',
+      'FloatProperties',
+      'IntProperties',
+      'StringProperties',
+    ];
+
+    for (const className of classNames) {
+      describe(`changing focus to ${className}`, () => {
+        let schema: Realm.ObjectSchema;
+
+        before(async () => {
+          // Click the class in the sidebar
+          await app.client.click(`span=${className}`);
+          const objectSchema = realm.schema.find(s => s.name === className);
+          assert(objectSchema, `${className} is missing from the schema`);
+          schema = objectSchema as Realm.ObjectSchema;
+        });
+
+        it('has the expected number of header cells', async () => {
+          // Assert something about the header
+          const headerCells = await app.client.elements(
+            '.RealmBrowser__Table__HeaderCell',
+          );
+          // Assert that is has the same number of header cells as it has properties
+          // The cell to add a property only has class __HeaderCellControl
+          assert.equal(
+            headerCells.value.length,
+            Object.keys(schema.properties).length,
+          );
+        });
+
+        it('has can create a row of defaults', async () => {
+          // Create a row
+          await app.client.click(`button=Create ${className}`);
+          await app.client.waitForVisible('button=Create');
+          await app.client.click('button=Create');
+          // Wait for the dialog is no longer open
+          await app.client.waitForExist('body:not(.modal-open)');
+        });
+
+        // Assert something about the row that was just created
+
+        it('created a row with the expected number of cells', async () => {
+          // +1 for the cell below the one that adds a property
+          const cells = await app.client.elements('.RealmBrowser__Table__Cell');
+          assert.equal(
+            cells.value.length,
+            Object.keys(schema.properties).length + 1,
+          );
+        });
+
+        /*
+        it('can focus and blur each cell', async () => {
+          const cells = await app.client.elements('.RealmBrowser__Table__Cell');
+          const propertyNames = Object.keys(schema.properties);
+          for (const cell of cells.value) {
+            // Click the cell
+            await app.client.elementIdClick(cell.ELEMENT);
+          }
+        });
+        */
+      });
+    }
+  });
+});
