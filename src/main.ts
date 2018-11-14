b9ad205031d1dec3c268c1635a211d1654bcd67e
Nikola Irinchev <irinchev@me.com>
2018-11-14 11:16:43 +0100

    Disable analytics in the main package

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,10 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
+// Don't report Realm JS analytics data
+// @see https://github.com/realm/realm-js/blob/master/lib/submit-analytics.js#L28
+process.env.REALM_DISABLE_ANALYTICS = 'true';
+
 import './sentry';
 
 import { app } from 'electron';
