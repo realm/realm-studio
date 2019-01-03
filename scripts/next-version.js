c971c5dacde6af1e5795dcf8713291dcd0efd19b
Kræn Hansen <mail@kraenhansen.dk>
2019-01-03 10:53:38 +0100

    Create next-version.js

---
+++ scripts/next-version.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+
+const releaseNotesPath = path.resolve(__dirname, '../RELEASENOTES.md');
+const releaseNotes = fs.readFileSync(releaseNotesPath, 'utf8');
+
+const hasBreakingChanges = /## Breaking Changes/.test(releaseNotes);
+const hasNoEnhancements = /## Enhancements\n- None/.test(releaseNotes);
+if (hasBreakingChanges) {
+  console.log("major");
+} else if (hasNoEnhancements) {
+  console.log("patch");
+} else {
+  console.log("minor");
+}
