6e6c1ab5520ffb83109cbc8e7565574421a3dbc9
Kræn Hansen <kraen.hansen@realm.io>
2019-10-18 22:19:43 +0200

    Fixed the next-version script

--- scripts/next-version.js
+++ scripts/next-version.js
@@ -6,7 +6,7 @@ const releaseNotesPath = path.resolve(__dirname, '../RELEASENOTES.md');
 const releaseNotes = fs.readFileSync(releaseNotesPath, 'utf8');
 
 const hasBreakingChanges = /## Breaking Changes/.test(releaseNotes);
-const hasNoEnhancements = /## Enhancements\n\n\* None/.test(releaseNotes);
+const hasNoEnhancements = /## Enhancements\n\n- None/.test(releaseNotes);
 if (hasBreakingChanges) {
   console.log('major');
 } else if (hasNoEnhancements) {
