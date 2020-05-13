115abb702328f91bc27c3561fbd27f4ac3e1b37a
Kræn Hansen <kraen.hansen@realm.io>
2020-05-13 09:03:45 +0200

    Removing everything related to ROS and RaaS (legacy Realm Cloud) (#1282)
    * Update product name and version
    
    * Removed everything related to ROS and RaaS
    
    * Adding a release note
    
    * Fixed linter errors

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,5 +1,10 @@
 [Changes since {PREVIOUS_VERSION}](https://github.com/realm/realm-studio/compare/{PREVIOUS_VERSION}...{CURRENT_VERSION})
 
+### Breaking Changes
+
+- All functionality related to the Realm Object Server (ROS), such as connecting to a server, opening synchronized Realms from it and managing users and their permissions has been removed. ([#1282](https://github.com/realm/realm-studio/pull/1282))
+- All functionality related to the "legacy" Realm Cloud (available via https://cloud.realm.io/), such as authenticating and connecting to a server instance has been removed. ([#1282](https://github.com/realm/realm-studio/pull/1282))
+
 ### Enhancements
 
 - None
--- src/ui/Greeting/index.tsx
+++ src/ui/Greeting/index.tsx
@@ -17,23 +17,17 @@
 ////////////////////////////////////////////////////////////////////////////
 
 import electron from 'electron';
-import qs from 'querystring';
 import React from 'react';
 
 import { main } from '../../actions/main';
-import { ICloudStatus, IInstance } from '../../main/CloudManager';
 import { IUpdateStatus } from '../../main/Updater';
-import * as raas from '../../services/raas';
 import { IMenuGeneratorProps } from '../../windows/MenuGenerator';
-import { showError } from '../reusable/errors';
 
-import { authenticate } from '../../services/ros/users';
 import { Greeting } from './Greeting';
 
 export type SocialNetwork = 'twitter' | 'facebook' | 'reddit' | 'hacker-news';
 
 interface IGreetingContainerState {
-  cloudStatus?: ICloudStatus;
   isCloudInstancesDropdownOpen: boolean;
   isSyncEnabled: boolean;
   updateStatus: IUpdateStatus;
@@ -54,9 +48,7 @@ class GreetingContainer extends React.Component<
   };
 
   public componentDidMount() {
-    electron.ipcRenderer.on('cloud-status', this.cloudStatusChanged);
     electron.ipcRenderer.on('update-status', this.updateStatusChanged);
-
     // Require realm and check update state with the sync support
     // Using nextTick to prevent blocking when loading realm
     process.nextTick(() => {
@@ -69,10 +61,6 @@ class GreetingContainer extends React.Component<
 
   public componentWillUnmount() {
     electron.ipcRenderer.removeListener(
-      'cloud-status',
-      this.cloudStatusChanged,
-    );
-    electron.ipcRenderer.removeListener(
       'update-status',
       this.updateStatusChanged,
     );
@@ -82,42 +70,6 @@ class GreetingContainer extends React.Component<
     return <Greeting {...this.state} {...this} />;
   }
 
-  public onAuthenticate = async () => {
-    await main.showCloudAuthentication();
-  };
-
-  public onCloudInstanceCreated = async (instance: IInstance) => {
-    await main.refreshCloudStatus();
-    this.onConnectToCloudInstance(instance);
-  };
-
-  public onConnectToCloudInstance = async (
-    instance: IInstance,
-  ): Promise<void> => {
-    try {
-      const tenantUrl = instance.tenantUrl;
-      if (tenantUrl) {
-        const credentials = raas.user.getTenantCredentials(tenantUrl);
-        const user = await authenticate(credentials);
-        return main.showServerAdministration({
-          user: user.serialize(),
-          validateCertificates: true,
-          isCloudTenant: true,
-        });
-      } else {
-        throw new Error(`Couldn't determine the URL of the server`);
-      }
-    } catch (err) {
-      showError('Failed to connect to the server', err);
-      // The cloud token might have been invalidated, refreshing the status will show this to the user.
-      await main.refreshCloudStatus();
-    }
-  };
-
-  public onConnectToServer = () => {
-    main.showConnectToServer();
-  };
-
   public onOpenLocalRealm = () => {
     main.showOpenLocalRealm();
   };
@@ -126,77 +78,12 @@ class GreetingContainer extends React.Component<
     main.checkForUpdates();
   };
 
-  public onDeauthenticate = () => {
-    main.deauthenticate();
-  };
-
   public updateStatusChanged = (
     e: Electron.IpcRendererEvent,
     status: IUpdateStatus,
   ) => {
     this.setState({ updateStatus: status });
   };
-
-  public cloudStatusChanged = (
-    e: Electron.IpcRendererEvent,
-    status: ICloudStatus,
-  ) => {
-    // Focus the window when the status requires the users attention
-    if (status.kind === 'authenticated' && status.justAuthenticated) {
-      electron.remote.getCurrentWindow().focus();
-    }
-    // Update the cloud status
-    this.setState({ cloudStatus: status });
-    this.props.updateMenu();
-  };
-
-  public onRefreshCloudStatus = () => {
-    main.refreshCloudStatus();
-  };
-
-  public onInstanceCreate = () => {
-    const endpoint = raas.getEndpoint();
-    electron.remote.shell.openExternal(`${endpoint}/instances/create`);
-  };
-
-  public onShare = (socialNetwork: SocialNetwork) => {
-    const url = this.getShareUrl(socialNetwork);
-    if (url) {
-      electron.shell.openExternal(url);
-    } else {
-      alert('We have not announced this yet');
-    }
-  };
-
-  public onToggleCloudInstancesDropdown = () => {
-    const { isCloudInstancesDropdownOpen } = this.state;
-    this.setState({
-      isCloudInstancesDropdownOpen: !isCloudInstancesDropdownOpen,
-    });
-  };
-
-  protected getShareUrl(socialNetwork: SocialNetwork) {
-    if (socialNetwork === 'twitter') {
-      // See https://dev.twitter.com/web/tweet-button for options
-      const query = qs.stringify({
-        text: 'Excited that Realm is creating a #cloud solution!',
-        via: 'realm',
-      });
-      return `https://twitter.com/intent/tweet?${query}`;
-    } else if (socialNetwork === 'facebook') {
-      const query = qs.stringify({
-        // TODO: Update this URL once we have announced it
-        u: encodeURI('https://realm.io/products/realm-cloud'),
-        display: 'page',
-      });
-      return `https://www.facebook.com/sharer/sharer.php?${query}`;
-    } else if (socialNetwork === 'reddit') {
-      // TODO: Make this a link to a post we've created
-    } else if (socialNetwork === 'hacker-news') {
-      // TODO: Make this a link to a post we've created
-      // https://news.ycombinator.com/item?id=15853477
-    }
-  }
 }
 
 export { GreetingContainer as Greeting };
