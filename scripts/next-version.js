5b6e470a5a0888bc54e24f07d07030e3ed3e37f9
Kræn Hansen <kraen.hansen@realm.io>
2020-06-04 09:20:43 +0200

    Fixed suggested next version for prereleases

--- scripts/next-version.js
+++ scripts/next-version.js
@@ -1,16 +1,23 @@
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const cp = require('child_process');
+const semver = require('semver');
 
 const releaseNotesPath = path.resolve(__dirname, '../RELEASENOTES.md');
 const releaseNotes = fs.readFileSync(releaseNotesPath, 'utf8');
+const packageJsonPath = path.resolve(__dirname, '../package.json')
+const packageJson = fs.readJSONSync(packageJsonPath);
 
-const hasBreakingChanges = /## Breaking Changes/.test(releaseNotes);
-const hasNoEnhancements = /## Enhancements\n\n- None/.test(releaseNotes);
-if (hasBreakingChanges) {
-  console.log('major');
-} else if (hasNoEnhancements) {
-  console.log('patch');
+if (semver.prerelease(packageJson.version) !== null) {
+  console.log('prerelease');
 } else {
-  console.log('minor');
+  const hasBreakingChanges = /## Breaking Changes/.test(releaseNotes);
+  const hasNoEnhancements = /## Enhancements\n\n- None/.test(releaseNotes);
+  if (hasBreakingChanges) {
+    console.log('major');
+  } else if (hasNoEnhancements) {
+    console.log('patch');
+  } else {
+    console.log('minor');
+  }
 }
