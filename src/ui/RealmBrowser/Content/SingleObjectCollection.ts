959d522955a0ed0f017bc5f8a92211e89bf088fd
Steffen Agger <steffen.agger@gmail.com>
2020-09-02 15:13:12 +0200

    jsdoc description for SingleObjectCollection

--- src/ui/RealmBrowser/Content/SingleObjectCollection.ts
+++ src/ui/RealmBrowser/Content/SingleObjectCollection.ts
@@ -16,6 +16,10 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
+/**
+ * Mock implementation of Realm.Result<T> to wrap a single Realm.Object, to allow
+ * using a single Realm.Object in the Table.
+ */
 export class SingleObjectCollection<T extends Realm.Object> extends Array<T>
   implements Realm.Results<T> {
   public readonly type: Realm.PropertyType;
