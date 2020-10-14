022c6f44f456e47cf851d5ec976cbce84ff71626
Steffen Agger <steffen.agger@gmail.com>
2020-10-14 16:15:43 +0200

    notarize.js ready for test-run

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,40 +1,45 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const { notarize } = require("electron-notarize");
 
-module.exports = async function notarizeTask () {
-  // TODO: remove this
-  console.log('notarizeTask should run now');
-  return;
+module.exports = async function notarizeApp (context) {
+  const { 
+    electronPlatformName,
+    appOutDir,
+    packager: {
+      appInfo: {
+        productFilename
+      }
+    }
+  } = context;
 
   // Only notarize the app on MacOS.
-  if (process.platform !== 'darwin') {
+  if (electronPlatformName !== "darwin") {
     return;
   }
 
-  console.log('electron-builder.afterSign hook triggered, now running notarizeTask', params);
+  console.log("electron-builder.afterSign hook triggered, now running notarizeApp");
 
+  // Options variables
   const appBundleId = "com.mongodb.realm-studio";
-  const appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
-  const appleId = "";
+  const appPath = path.join(appOutDir, `${productFilename}.app`);
+  const appleId = "[APPLE ID PENDING]";
   const appleIdPassword = process.env.GITHUB_ACTION
-    ? process.env.APPLE_ID_APP_PASSWORD // must be defined as the environment variable "APPLE_ID_APP_PASSWORD" for GitHub Actions
+    ? process.env.APPLE_ID_APP_PASSWORD
     : `@keychain:"Application Loader: ${appleId}"`;
-  // NOTE: can be found with:
+  const ascProvider = "[TEAM PENDING]";
+  // TODO: find ascProvider with:
   // /Applications/Transporter.app/Contents/itms/bin/iTMSTransporter -m provider -u APPLE_DEV_ACCOUNT -p APP_PASSWORD
-  const ascProvider = "MongoDB Realm";
 
+  const options = { appBundleId, appPath, appleId, appleIdPassword, ascProvider };
+
+  // Sanity check
   if (!fs.existsSync(appPath)) {
-    throw new Error(`Cannot find application at: ${appPath}`);
+    throw new Error(`Cannot find application: ${appPath}`);
   }
 
-  await notarize({
-    appBundleId,
-    appPath,
-    appleId,
-    appleIdPassword,
-    ascProvider
-  });
+  // await notarize(options);
+  console.log("[SKIPPING] notarization of app! options:", options);
 
-  console.log(`Done notarizing ${appId}`);
+  console.log(`Done notarizing ${appBundleId}`);
 };
