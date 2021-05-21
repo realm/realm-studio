2225855d399f5125afa4bd52473bec68fcf7be4a
Steffen Agger <steffen.agger@gmail.com>
2021-05-21 14:46:15 +0200

    "Download a demo Realm file" link now points to demo-v21.realm

--- src/ui/Greeting/Greeting.tsx
+++ src/ui/Greeting/Greeting.tsx
@@ -63,7 +63,7 @@ export const Greeting = ({
       <div className="Greeting__DownloadDemo">
         <span>New to realm? </span>
         <a
-          href="https://static.realm.io/downloads/realm-studio/demo-v20.realm"
+          href="https://static.realm.io/downloads/realm-studio/demo-v21.realm"
           className="Link"
         >
           Download a demo Realm file
